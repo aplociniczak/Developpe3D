49bf29f10b126a185e75355e8938f68857f9ee85
Alexandre Plociniczak <alexplo@hotmail.fr>
2020-01-26 22:53:16 +0100

    Ajout de la fonctionnalité de découpage

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,90 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.B_Browse_VT = new System.Windows.Forms.Button();
             this.FilePath_B1 = new System.Windows.Forms.TextBox();
             this.FileName_B1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.B_Browse_P3D = new System.Windows.Forms.Button();
             this.FilePath_B2 = new System.Windows.Forms.TextBox();
             this.FileName_B2 = new System.Windows.Forms.TextBox();
-            this.Img_VT = new System.Windows.Forms.PictureBox();
-            this.Img_P3D = new System.Windows.Forms.PictureBox();
+            this.PB_IMG_VT = new System.Windows.Forms.PictureBox();
+            this.PB_IMG_P3D = new System.Windows.Forms.PictureBox();
             this.Calcul_b1 = new System.Windows.Forms.Button();
             this.Calcul_b2 = new System.Windows.Forms.Button();
             this.b_quit = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.Img_VT)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Img_P3D)).BeginInit();
+            this.PB_Preview_VT = new System.Windows.Forms.PictureBox();
+            this.PB_Preview_P3D = new System.Windows.Forms.PictureBox();
+            this.Select_Area_VT = new System.Windows.Forms.Button();
+            this.Crop_P3D = new System.Windows.Forms.Button();
+            this.Crop_VT = new System.Windows.Forms.Button();
+            this.Select_Area_P3D = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_IMG_VT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_IMG_P3D)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Preview_VT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Preview_P3D)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // B_Browse_VT
             // 
-            this.button1.Location = new System.Drawing.Point(120, 233);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Parcourir...";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.B_Browse_VT.Location = new System.Drawing.Point(34, 223);
+            this.B_Browse_VT.Name = "B_Browse_VT";
+            this.B_Browse_VT.Size = new System.Drawing.Size(161, 40);
+            this.B_Browse_VT.TabIndex = 0;
+            this.B_Browse_VT.Text = "Parcourir...";
+            this.B_Browse_VT.UseVisualStyleBackColor = true;
+            this.B_Browse_VT.Click += new System.EventHandler(this.button1_Click);
             // 
             // FilePath_B1
             // 
-            this.FilePath_B1.Location = new System.Drawing.Point(120, 279);
+            this.FilePath_B1.Location = new System.Drawing.Point(34, 269);
             this.FilePath_B1.Name = "FilePath_B1";
             this.FilePath_B1.Size = new System.Drawing.Size(423, 20);
             this.FilePath_B1.TabIndex = 1;
             // 
             // FileName_B1
             // 
-            this.FileName_B1.Location = new System.Drawing.Point(287, 233);
+            this.FileName_B1.Location = new System.Drawing.Point(201, 223);
             this.FileName_B1.Name = "FileName_B1";
             this.FileName_B1.Size = new System.Drawing.Size(256, 20);
             this.FileName_B1.TabIndex = 1;
             // 
-            // button2
+            // B_Browse_P3D
             // 
-            this.button2.Location = new System.Drawing.Point(120, 593);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(161, 40);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Parcourir...";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.B_Browse_P3D.Location = new System.Drawing.Point(34, 563);
+            this.B_Browse_P3D.Name = "B_Browse_P3D";
+            this.B_Browse_P3D.Size = new System.Drawing.Size(161, 40);
+            this.B_Browse_P3D.TabIndex = 2;
+            this.B_Browse_P3D.Text = "Parcourir...";
+            this.B_Browse_P3D.UseVisualStyleBackColor = true;
+            this.B_Browse_P3D.Click += new System.EventHandler(this.button2_Click);
             // 
             // FilePath_B2
             // 
-            this.FilePath_B2.Location = new System.Drawing.Point(120, 650);
+            this.FilePath_B2.Location = new System.Drawing.Point(34, 640);
             this.FilePath_B2.Name = "FilePath_B2";
             this.FilePath_B2.Size = new System.Drawing.Size(423, 20);
             this.FilePath_B2.TabIndex = 3;
             // 
             // FileName_B2
             // 
-            this.FileName_B2.Location = new System.Drawing.Point(287, 593);
+            this.FileName_B2.Location = new System.Drawing.Point(201, 563);
             this.FileName_B2.Name = "FileName_B2";
             this.FileName_B2.Size = new System.Drawing.Size(256, 20);
             this.FileName_B2.TabIndex = 3;
             // 
-            // Img_VT
+            // PB_IMG_VT
             // 
-            this.Img_VT.Location = new System.Drawing.Point(684, 105);
-            this.Img_VT.Name = "Img_VT";
-            this.Img_VT.Size = new System.Drawing.Size(631, 318);
-            this.Img_VT.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Img_VT.TabIndex = 4;
-            this.Img_VT.TabStop = false;
+            this.PB_IMG_VT.BackColor = System.Drawing.Color.White;
+            this.PB_IMG_VT.Location = new System.Drawing.Point(517, 115);
+            this.PB_IMG_VT.Name = "PB_IMG_VT";
+            this.PB_IMG_VT.Size = new System.Drawing.Size(631, 318);
+            this.PB_IMG_VT.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_IMG_VT.TabIndex = 4;
+            this.PB_IMG_VT.TabStop = false;
+            this.PB_IMG_VT.Click += new System.EventHandler(this.PB_IMG_VT_Click);
             // 
-            // Img_P3D
+            // PB_IMG_P3D
             // 
-            this.Img_P3D.Location = new System.Drawing.Point(684, 476);
-            this.Img_P3D.Name = "Img_P3D";
-            this.Img_P3D.Size = new System.Drawing.Size(631, 318);
-            this.Img_P3D.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.Img_P3D.TabIndex = 5;
-            this.Img_P3D.TabStop = false;
+            this.PB_IMG_P3D.BackColor = System.Drawing.Color.White;
+            this.PB_IMG_P3D.Location = new System.Drawing.Point(517, 491);
+            this.PB_IMG_P3D.Name = "PB_IMG_P3D";
+            this.PB_IMG_P3D.Size = new System.Drawing.Size(631, 318);
+            this.PB_IMG_P3D.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_IMG_P3D.TabIndex = 5;
+            this.PB_IMG_P3D.TabStop = false;
             // 
             // Calcul_b1
             // 
-            this.Calcul_b1.Location = new System.Drawing.Point(120, 338);
+            this.Calcul_b1.Location = new System.Drawing.Point(34, 328);
             this.Calcul_b1.Name = "Calcul_b1";
             this.Calcul_b1.Size = new System.Drawing.Size(423, 41);
             this.Calcul_b1.TabIndex = 6;
@@ -121,7 +132,7 @@
             // 
             // Calcul_b2
             // 
-            this.Calcul_b2.Location = new System.Drawing.Point(120, 698);
+            this.Calcul_b2.Location = new System.Drawing.Point(34, 688);
             this.Calcul_b2.Name = "Calcul_b2";
             this.Calcul_b2.Size = new System.Drawing.Size(423, 41);
             this.Calcul_b2.TabIndex = 6;
@@ -139,27 +150,95 @@
             this.b_quit.UseVisualStyleBackColor = true;
             this.b_quit.Click += new System.EventHandler(this.button3_Click_1);
             // 
+            // PB_Preview_VT
+            // 
+            this.PB_Preview_VT.BackColor = System.Drawing.Color.ForestGreen;
+            this.PB_Preview_VT.Location = new System.Drawing.Point(1199, 115);
+            this.PB_Preview_VT.Name = "PB_Preview_VT";
+            this.PB_Preview_VT.Size = new System.Drawing.Size(139, 158);
+            this.PB_Preview_VT.TabIndex = 8;
+            this.PB_Preview_VT.TabStop = false;
+            this.PB_Preview_VT.Click += new System.EventHandler(this.preview_VT_Click);
+            // 
+            // PB_Preview_P3D
+            // 
+            this.PB_Preview_P3D.BackColor = System.Drawing.Color.ForestGreen;
+            this.PB_Preview_P3D.Location = new System.Drawing.Point(1199, 491);
+            this.PB_Preview_P3D.Name = "PB_Preview_P3D";
+            this.PB_Preview_P3D.Size = new System.Drawing.Size(139, 158);
+            this.PB_Preview_P3D.TabIndex = 9;
+            this.PB_Preview_P3D.TabStop = false;
+            this.PB_Preview_P3D.Click += new System.EventHandler(this.preview_P3D_Click);
+            // 
+            // Select_Area_VT
+            // 
+            this.Select_Area_VT.Location = new System.Drawing.Point(1199, 337);
+            this.Select_Area_VT.Name = "Select_Area_VT";
+            this.Select_Area_VT.Size = new System.Drawing.Size(139, 23);
+            this.Select_Area_VT.TabIndex = 10;
+            this.Select_Area_VT.Text = "Select Area";
+            this.Select_Area_VT.UseVisualStyleBackColor = true;
+            this.Select_Area_VT.Click += new System.EventHandler(this.button3_Click_2);
+            // 
+            // Crop_P3D
+            // 
+            this.Crop_P3D.Location = new System.Drawing.Point(1199, 722);
+            this.Crop_P3D.Name = "Crop_P3D";
+            this.Crop_P3D.Size = new System.Drawing.Size(139, 23);
+            this.Crop_P3D.TabIndex = 11;
+            this.Crop_P3D.Text = "Crop Area";
+            this.Crop_P3D.UseVisualStyleBackColor = true;
+            this.Crop_P3D.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Crop_VT
+            // 
+            this.Crop_VT.Location = new System.Drawing.Point(1199, 366);
+            this.Crop_VT.Name = "Crop_VT";
+            this.Crop_VT.Size = new System.Drawing.Size(139, 23);
+            this.Crop_VT.TabIndex = 12;
+            this.Crop_VT.Text = "Crop Area";
+            this.Crop_VT.UseVisualStyleBackColor = true;
+            this.Crop_VT.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Select_Area_P3D
+            // 
+            this.Select_Area_P3D.Location = new System.Drawing.Point(1199, 688);
+            this.Select_Area_P3D.Name = "Select_Area_P3D";
+            this.Select_Area_P3D.Size = new System.Drawing.Size(139, 23);
+            this.Select_Area_P3D.TabIndex = 13;
+            this.Select_Area_P3D.Text = "Select Area";
+            this.Select_Area_P3D.UseVisualStyleBackColor = true;
+            this.Select_Area_P3D.Click += new System.EventHandler(this.Select_Area_P3D_Click);
+            // 
             // Developpe3D
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1453, 903);
+            this.Controls.Add(this.Select_Area_P3D);
+            this.Controls.Add(this.Crop_VT);
+            this.Controls.Add(this.Crop_P3D);
+            this.Controls.Add(this.Select_Area_VT);
+            this.Controls.Add(this.PB_Preview_P3D);
+            this.Controls.Add(this.PB_Preview_VT);
             this.Controls.Add(this.b_quit);
             this.Controls.Add(this.Calcul_b2);
             this.Controls.Add(this.Calcul_b1);
-            this.Controls.Add(this.Img_P3D);
-            this.Controls.Add(this.Img_VT);
+            this.Controls.Add(this.PB_IMG_P3D);
+            this.Controls.Add(this.PB_IMG_VT);
             this.Controls.Add(this.FileName_B2);
             this.Controls.Add(this.FilePath_B2);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.B_Browse_P3D);
             this.Controls.Add(this.FileName_B1);
             this.Controls.Add(this.FilePath_B1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.B_Browse_VT);
             this.Name = "Developpe3D";
             this.Text = "Developpe3D";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.Img_VT)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Img_P3D)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_IMG_VT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_IMG_P3D)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Preview_VT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Preview_P3D)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,17 +246,23 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button B_Browse_VT;
         private System.Windows.Forms.TextBox FilePath_B1;
         private System.Windows.Forms.TextBox FileName_B1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button B_Browse_P3D;
         private System.Windows.Forms.TextBox FilePath_B2;
         private System.Windows.Forms.TextBox FileName_B2;
-        private System.Windows.Forms.PictureBox Img_VT;
-        private System.Windows.Forms.PictureBox Img_P3D;
+        private System.Windows.Forms.PictureBox PB_IMG_VT;
+        private System.Windows.Forms.PictureBox PB_IMG_P3D;
         private System.Windows.Forms.Button Calcul_b1;
         private System.Windows.Forms.Button Calcul_b2;
         private System.Windows.Forms.Button b_quit;
+        private System.Windows.Forms.PictureBox PB_Preview_VT;
+        private System.Windows.Forms.PictureBox PB_Preview_P3D;
+        private System.Windows.Forms.Button Select_Area_VT;
+        private System.Windows.Forms.Button Crop_P3D;
+        private System.Windows.Forms.Button Crop_VT;
+        private System.Windows.Forms.Button Select_Area_P3D;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -28,8 +28,11 @@ namespace Developpe3D
             {                
                 FilePath_B1.Text = ofd_b1.FileName;
                 FileName_B1.Text = ofd_b1.SafeFileName;
-                Img_VT.ImageLocation = ofd_b1.FileName;
+                PB_IMG_VT.ImageLocation = ofd_b1.FileName;
             }
+
+            Image img_vt = new Bitmap(ofd_b1.FileName);
+            PB_IMG_VT.Image = img_vt;
 
 
         }
@@ -44,8 +47,11 @@ namespace Developpe3D
             {
                 FilePath_B2.Text = ofd_b2.FileName;
                 FileName_B2.Text = ofd_b2.SafeFileName;
-                Img_P3D.ImageLocation = ofd_b2.FileName;
+                PB_IMG_P3D.ImageLocation = ofd_b2.FileName;
             }
+
+            Image img_p3d = new Bitmap(ofd_b2.FileName);
+            PB_IMG_P3D.Image = img_p3d;
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -66,6 +72,155 @@ namespace Developpe3D
         private void button3_Click_1(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_2(object sender, EventArgs e)
+        {
+            PB_IMG_VT.MouseDown += new MouseEventHandler(PB_MouseDown);
+            PB_IMG_VT.MouseMove += new MouseEventHandler(PB_VT_MouseMove);
+            PB_IMG_VT.MouseEnter += new EventHandler(PB_MouseEnter);
+            Controls.Add(PB_IMG_VT);
+        }
+
+        int crpX, crpY, rectW, rectH;
+
+        public Pen crpPen = new Pen(Color.Blue);
+
+        private void PB_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Cursor = Cursors.Cross;
+                crpPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                // set initial x,y co ordinates for crop rectangle
+                //this is where we firstly click on image
+                crpX = e.X;
+                crpY = e.Y;
+
+            }
+        }
+
+        
+
+        private void preview_VT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+            //Now we will draw the cropped image into pictureBox2
+            Bitmap bmp2 = new Bitmap(PB_IMG_VT.Width, PB_IMG_VT.Height);
+            PB_IMG_VT.DrawToBitmap(bmp2, PB_IMG_VT.ClientRectangle);
+
+            Bitmap crpImg = new Bitmap(rectW, rectH);
+
+            for (int i = 0; i < rectW; i++)
+            {
+                for (int y = 0; y < rectH; y++)
+                {
+                    Color pxlclr = bmp2.GetPixel(crpX + i, crpY + y);
+                    crpImg.SetPixel(i, y, pxlclr);
+                }
+            }
+
+            PB_Preview_VT.Image = (Image)crpImg;
+            PB_Preview_VT.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void PB_MouseEnter(object sender, EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Cross;
+        }
+
+        private void PB_VT_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                PB_IMG_VT.Refresh();
+                //set width and height for crop rectangle.
+                rectW = e.X - crpX;
+                rectH = e.Y - crpY;
+                Graphics g = PB_IMG_VT.CreateGraphics();
+                g.DrawRectangle(crpPen, crpX, crpY, rectW, rectH);
+                g.Dispose();
+            }
+        }
+
+        private void PB_P3D_MouseMove(object sender, MouseEventArgs e)            
+        {
+            base.OnMouseMove(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                PB_IMG_P3D.Refresh();
+                //set width and height for crop rectangle.
+                rectW = e.X - crpX;
+                rectH = e.Y - crpY;
+                Graphics g = PB_IMG_P3D.CreateGraphics();
+                g.DrawRectangle(crpPen, crpX, crpY, rectW, rectH);
+                g.Dispose();
+            }
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Default;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Select_Area_P3D_Click(object sender, EventArgs e)
+        {
+            PB_IMG_P3D.MouseDown += new MouseEventHandler(PB_MouseDown);
+            PB_IMG_P3D.MouseMove += new MouseEventHandler(PB_P3D_MouseMove);
+            PB_IMG_P3D.MouseEnter += new EventHandler(PB_MouseEnter);
+            Controls.Add(PB_IMG_P3D);
+        }
+
+        private void preview_P3D_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+            //Now we will draw the cropped image into pictureBox2
+            Bitmap bmp3 = new Bitmap(PB_IMG_P3D.Width, PB_IMG_P3D.Height);
+            PB_IMG_P3D.DrawToBitmap(bmp3, PB_IMG_P3D.ClientRectangle);
+
+            Bitmap crpImg = new Bitmap(rectW, rectH);
+
+            for (int i = 0; i < rectW; i++)
+            {
+                for (int y = 0; y < rectH; y++)
+                {
+                    Color pxlclr = bmp3.GetPixel(crpX + i, crpY + y);
+                    crpImg.SetPixel(i, y, pxlclr);
+                }
+            }
+
+            PB_Preview_P3D.Image = (Image)crpImg;
+            PB_Preview_P3D.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void PB_IMG_VT_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
